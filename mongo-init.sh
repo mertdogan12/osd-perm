b96fec4c179d7f2e91a0d381a9b13459191d977a
Mert Dogan <mert.dogan@me.com>
2022-04-23 00:58:38 +0200

    mongo docker update

---
+++ mongo-init.sh
@@ -0,0 +1,18 @@
+set -e
+
+mongo <<EOF
+
+use $MONGO_DATABASE
+
+db.users.insertOne({
+  id: "$OSD_ID",
+  name: "$OSD_NAME",
+  groups: ["admins"],
+})
+
+db.groups.insertOne({
+  name: "admin",
+  permissions: ["*"],
+})
+
+EOF
